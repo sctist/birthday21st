4a728e10befedbb453286371491190241c7012da
sctist <159453670+sctist@users.noreply.github.com>
2024-02-08 21:21:47 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.birthdayCard').css('display', 'none');
+    $('.canvas').css('display', 'none');
+});
+
+function play(){
+    $('.button').css('display', 'none');
+    $('.birthdayCard').css('display', 'block');
+}
