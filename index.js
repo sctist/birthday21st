180eb5fd6f92e431aba44b74bba8885f1fa3d922
sctist <159453670+sctist@users.noreply.github.com>
2024-02-08 23:35:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-$(document).ready(function(){
-    $('.birthdayCard').css('display', 'none');
-    $('.canvas').css('display', 'none');
-});
-
 function play(){
+    var audio = new Audio();
+    audio.src = './A Thousand Years.mp3';
+    audio.play();  // Set autoplay to true
     $('.button').css('display', 'none');
     $('.birthdayCard').css('display', 'block');
 }
